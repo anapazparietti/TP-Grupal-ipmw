8562d08920c8fa11e33c1ffe81bcc94e065374bf
ana <anapazparietti@gmail.com>
2024-11-10 20:16:52 -0300

    comentarios borrados y función en uso

--- tpfinalparte1/funciones.js
+++ tpfinalparte1/funciones.js
@@ -1,18 +1,18 @@
-//textoPerzonalizado en cargarPantalla
-//remplazar todo
 //ver como hace que solo tengamos que escribir boton una vez, preguntar al profe x nuestra situacion
+//cómo hacemos para achicar la parte de créditos
 
 //-----------PANTALLAS-------------------
-function cargarPantalla(imagenes, texto, x, y, alineacion, rectTam) {//borrar ancho y alto en la función
+function cargarPantalla(imagenes, texto, x, y, xT, yT, tamT, tono, borde, alineacion, aliT, rectTam) {//borrar ancho y alto en la función
   imageMode(alineacion);
   image(imagenes, x, y);
   noStroke();
   fill(0, 150);
   rect(x, y, width, rectTam);//parametro para modificar alto del cuadrado dependiendo el alto de tu texto
-  textAlign(LEFT);
-  textSize(20);
-  fill(255);
-  text(texto, x+30, y+20, width-50);
+  stroke(borde);
+  textAlign(aliT);
+  textSize(tamT);
+  fill(tono);
+  text(texto, xT, yT, width-50);
   
 
 }
@@ -25,16 +25,6 @@ function textoPersonalizado(texto, x, y, tam, tono, borde, alineacion) {
   textAlign(alineacion);
   text(texto, x, y);
   pop();
-}
-
-function escribirParrafos(texto) {
-  noStroke();
-  fill(0, 150);
-  rect(0, 0, width, 165);
-  textAlign(LEFT);
-  textSize(20);
-  fill(255);
-  text(texto, 30, 20, width-50);
 }
 
 //--------------BOTONES----------------
